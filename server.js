b3f6380f31810788a64eacbb03521cbb0cc38545
teohen <teodorohenriquecarvalho@gmail.com>
2020-11-03 13:24:39 -0300

    feat(stream with large files): using streams to deal with large csv files

--- server.js
+++ server.js
@@ -1,37 +1,38 @@
 const fs = require('fs');
+var os = require("os");
 const CsvReadableStream = require('csv-reader');
-const http = require('http')
 
-
-const server =  http.createServer((req, res) => {
-    
-})
- 
-let inputStream = fs.createReadStream('TUP.csv', 'utf8');
-
-let tupDestinationStream = fs.createWriteStream('./orelhao_nordeste.json');
-
-inputStream
-    .pipe(new CsvReadableStream({ parseNumbers: true, parseBooleans: true, trim: true }))
-    .on('open', function(){
-        console.log('open readable')
+    let inputStream = fs.createReadStream('data/TUP.csv', 'utf8');
+    const writeStreams = new Map()
+    inputStream
+    .on('open', function () {
+        console.log('Read Stream open!')
     })
+    .pipe(new CsvReadableStream({ parseNumbers: true, parseBooleans: true, trim: true }))
     .on('data', function (row) {        
-        if (row[9] == 'MA') {
-            let writableObject = {
-                UF: row[9],
-                latitude: row[19],
-                longitute: row[20],
-                numero: row[6],
-                localidade: row[10]
-            }
-            //tupDestinationStream.write(JSON.stringify(writableObject));
-            //tupDestinationStream.write('//n');
-
+        console.log('processing data')
+        const stateUF = row[9]
+        if(stateUF.length == 2 ){
+            const writeStream = writableStreamManager(stateUF)
+            writeStream.write(row.toString())
+            writeStream.write(os.EOL)   
         }
     })
-    .on('end', function (data) {
-        console.log('No more rows!');
-    });
+    .on('end', () => {
+        console.log('Ended')
+    })
+    .on('error', (err) => {
+        console.log('General on Reading Streams')
+        console.log(err)
+    })
 
 
+function writableStreamManager(key){
+    let writableStream = writeStreams.get(key)
+    if (!writableStream)
+        writableStream = fs.createWriteStream(`${__dirname}/data/${key}.csv`, 'utf-8')
+
+    writeStreams.set(key, writableStream)
+
+    return writableStream
+}
